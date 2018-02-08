04bf359f1dc0cf15d84d7374ccdfa91148c8ef66
[iRoni10002] <[superpo4ta.rom@gmail.com]>
2018-02-08 16:02:01 +0600

    continue of 3 task

--- news_blog/models.py
+++ news_blog/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.utils import timezone
+import uuid
 
+
+class Author(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -8,5 +17,17 @@ class Article(models.Model):
     text = models.TextField()
     published_date = models.DateTimeField(
         blank=True, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def remove(self):
+        self.delete()
+
+    def __str__(self):
+        return '{} {}'. format(self.title, self.created_date)
 
 # Create your models here.
--- news_blog/views.py
+++ news_blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseNotFound
 from .models import Article
 
 
@@ -8,9 +8,14 @@ def index(request):
     return render(request, 'news_blog/news_list.html', {'articles':articles})
 
 def get_article(request, article_id):
-    articles = Article.objects.all()
-    article = articles[article_id - 1]
+    try:
+        article = Article.objects.get(id=article_id)
+    except Article.DoesNotExist:
+        return HttpResponseNotFound()
     return render(request, 'news_blog/news_article.html', {'article': article})
+    #articles = Article.objects.all()
+    #article = articles[article_id - 1]
+    #return render(request, 'news_blog/news_article.html', {'article': article})
 
 def about_autor(reqest):
     return render(reqest, 'news_blog/about.html')
